fe654b361865c0b7db8bfd8dd05f00b89c5c0e63
Nahid51 <hasannahidnahid@gmail.com>
2022-04-08 01:25:44 +0600

    added navbar and hero section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Hero.css";
+import { Button, createTheme, ThemeProvider, Typography } from '@mui/material';
+
+import { Autoplay, Pagination, Navigation, Virtual } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/virtual';
+import { NavLink } from "react-router-dom";
+
+
+const Hero = () => {
+    const [animeData, setAnimeData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://ghibliapi.herokuapp.com/films")
+            .then(res => res.json())
+            .then(data => setAnimeData(data))
+    }, []);
+
+    const theme = createTheme();
+
+    theme.typography.h1 = {
+        fontSize: '2rem',
+        '@media (min-width:480px) and (max-width:600px)': {
+            fontSize: '2rem',
+        },
+        '@media (max-width:480px)': {
+            fontSize: '1.5rem',
+        },
+        [theme.breakpoints.up('md')]: {
+            fontSize: '3rem',
+        },
+    };
+
+    return (
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                loop={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation, Virtual]}
+                className="mySwiper"
+            >
+                {
+                    animeData?.map((data, index) => (
+                        // console.log(data?.image)
+                        <SwiperSlide key={index} virtualIndex={index}>
+                            <div>
+                                <img src={data?.movie_banner} alt="Poster" />
+                                <div className="setPosition">
+                                    <ThemeProvider theme={theme}>
+                                        <Typography variant="h1">{data?.title}</Typography>
+                                    </ThemeProvider>
+                                    <NavLink to={`/singleAnime/${data?.id}`} style={{ textDecoration: "none" }}>
+                                        <Button
+                                            style={{
+                                                backgroundColor: "#592e97"
+                                            }}
+                                            variant="contained"
+                                            size="small"
+                                        >
+                                            Watch Details</Button>
+                                    </NavLink>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+
+            </Swiper>
+        </>
+    );
+};
+
+export default Hero;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+
+const Navbar = () => {
+
+    return (
+        <AppBar sx={{ backgroundColor: "black" }} position="static">
+            <Container maxWidth="lg">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontWeight: "bold" }}
+                    >
+                        AnimeWeb
+                    </Typography>
+
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                    >
+                        AnimeWeb
+                    </Typography>
+
+
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+export default Navbar;
