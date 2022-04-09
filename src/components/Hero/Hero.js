4b5e60e02a7a17f6faa110f91395af6becc91838
Nahid51 <hasannahidnahid@gmail.com>
2022-04-09 14:52:03 +0600

    modify some code

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Hero.css";
 import { Button, createTheme, ThemeProvider, Typography } from '@mui/material';
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { Avatar, Box, IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
+import { Avatar, Box, IconButton, Tooltip } from '@mui/material';
 
 
 const Navbar = () => {
